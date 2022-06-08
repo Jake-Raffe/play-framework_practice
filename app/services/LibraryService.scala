f33cf8eaa5b444f67953829fa31c613b59de5306
Jake-Raffe <jakeraffe24@gmail.com>
2022-06-08 10:53:30 +0100

    Fix errors in get in LibraryConnector, getGoogleBook in LibraryService, and getGoogleBook in ApplicationController

--- app/services/LibraryService.scala
+++ app/services/LibraryService.scala
@@ -13,7 +13,7 @@ import scala.concurrent.{ExecutionContext, Future}
 @Singleton
 class LibraryService @Inject()(connector: LibraryConnector){
 
-  def getGoogleBook(urlOverride: Option[String] = None, search: String, term: String)(implicit ec: ExecutionContext): EitherT[Future, APIError, Book] =
+  def getGoogleBook(urlOverride: Option[String] = None, search: String, term: String)(implicit ec: ExecutionContext): EitherT[Future, APIError, DataModel] =
    connector.get[DataModel](urlOverride.getOrElse(s"https://www.googleapis.com/books/v1/volumes?q=$search%$term"))
 
 
